5e2dbbdcfa67a04b4e11f30dcaefd02633122730
dmytrolr <dmytrolr88@gmail.com>
2025-08-15 10:00:11 +0300

    Оновлено виконані завдання

--- Homework 19/19.3.py
+++ Homework 19/19.3.py
@@ -7,3 +7,11 @@ class MyIterable:
 
     def __getitem__(self, index):
         return self.data[index]
+
+# obj = MyIterable(['a', 'b', 'c'])
+#
+# for item in obj:
+#     print(item)
+#
+# print(obj[0])
+# print(obj[2])
